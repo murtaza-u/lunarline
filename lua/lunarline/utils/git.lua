553f1f4d520a02db7236a8fc3676b90554491143
Murtaza Udaipurwal <murtaza@machine>
2021-07-17 16:58:41 +0530

    fixed bug which caused current_working_directory to be nil

--- lua/lunarline/utils/git.lua
+++ lua/lunarline/utils/git.lua
@@ -1,18 +1,19 @@
 local function get_parent_dir(cwd)
-    local i = cwd:find("[\\/:][^\\/:]*$")
+    local i = string.find(cwd, "[\\/:][^\\/:]*$")
     if not i then return end
     return cwd:sub(1, i-1)
 end
 
 local function main(cwd)
-    if vim.fn.finddir('.git', cwd) then
+    if vim.fn.finddir('.git', cwd) == '.git' then
         return true
     else
         cwd = get_parent_dir(cwd)
         if cwd == nil then
             return false
+        else
+            return main(cwd)
         end
-        return main(cwd)
     end
 end
 
--- lua/lunarline/utils/req_components.lua
+++ lua/lunarline/utils/req_components.lua
@@ -7,19 +7,18 @@ local all_components = {
     cursor_position = 'cursor_position',
     line_col = 'line_col',
 }
-local is_git_repo = require('lunarline.utils.git')
-
 local function req_components(options)
+    local is_git_repo = require('lunarline.utils.git')
     local components = {}
     for key, _ in pairs(all_components) do
         local new_key = "get_" .. key
         if options == nil then
-            if key == "git_diff" and not is_git_repo then
+            if (key == "git_diff" or key == "git_branch") and not is_git_repo then
                 goto continue
             end
             components[new_key] = require('lunarline.components.' .. key)
         elseif options[key] == true or options[key] == nil then
-            if key == "git_diff" and not is_git_repo then
+            if (key == "git_diff" or key == "git_branch") and not is_git_repo then
                 goto continue
             end
             components[new_key] = require('lunarline.components.' .. key)
