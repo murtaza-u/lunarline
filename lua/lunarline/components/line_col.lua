10bd43254c6c29e9782fb904771d0fa0f1bcdfab
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-07-18 18:41:47 +0530

    Added adequate padding on either side of line_col

--- lua/lunarline/components/line_col.lua
+++ lua/lunarline/components/line_col.lua
@@ -1,6 +1,6 @@
 local function get_line_col()
     local color = '%#line_col#'
-    return string.format("%s%s", color, "%l:%c")
+    return string.format("%s%s", color, "%2l:%-2c")
 end
 
 return get_line_col
